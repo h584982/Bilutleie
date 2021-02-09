823876c8f2553bd5950b1114e5e25060145f8d7d
thomaseikhaug <thomaseikhaug@192.168.10.121>
2021-02-09 15:04:38 +0100

    get og set metoder i Bil

--- src/bilutleie/Bil.java
+++ src/bilutleie/Bil.java
@@ -7,7 +7,54 @@ public class Bil {
 	private String modell;
 	private String farge;
 	private char utleiegruppe;
-	private boolean ledig; 
-	
-	
+	private boolean ledig;
+
+	public int getRegistreringsnummer() {
+		return registreringsnummer;
+	}
+
+	public void setRegistreringsnummer(int registreringsnummer) {
+		this.registreringsnummer = registreringsnummer;
+	}
+
+	public String getMerke() {
+		return merke;
+	}
+
+	public void setMerke(String merke) {
+		this.merke = merke;
+	}
+
+	public String getModell() {
+		return modell;
+	}
+
+	public void setModell(String modell) {
+		this.modell = modell;
+	}
+
+	public String getFarge() {
+		return farge;
+	}
+
+	public void setFarge(String farge) {
+		this.farge = farge;
+	}
+
+	public char getUtleiegruppe() {
+		return utleiegruppe;
+	}
+
+	public void setUtleiegruppe(char utleiegruppe) {
+		this.utleiegruppe = utleiegruppe;
+	}
+
+	public boolean isLedig() {
+		return ledig;
+	}
+
+	public void setLedig(boolean ledig) {
+		this.ledig = ledig;
+	}
+
 }
--- src/bilutleie/Bilutleieselskap.java
+++ src/bilutleie/Bilutleieselskap.java
@@ -5,6 +5,4 @@ public class Bilutleieselskap {
 	private String navn;
 	private int telefonnummer;
 	private String firmaAdresse;
-	
-	
 }
