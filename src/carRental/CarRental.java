208e4750b2b2413ab70707cd31ffe2a009cffe8a
Jogeir Liavåg <jogeir@tutanota.com>
2021-02-17 12:09:23 +0100

    Implemented Shell Classes from Simen

---
+++ src/carRental/CarRental.java
@@ -0,0 +1,101 @@
+package carRental;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class CarRental {
+
+	private String name;
+	private int phoneNumber;
+	private ArrayList<Customer> customers;
+	private ArrayList<RentalOffice> offices;
+	private Address address;
+	
+	public CarRental(String name, int phoneNumber, ArrayList<Customer> customers, ArrayList<RentalOffice> offices,
+			Address address) {
+		super();
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.customers = customers;
+		this.offices = offices;
+		this.address = address;
+	}
+	
+	public CarRental() {
+		
+	}
+	
+	public void createCustomer(String firstName, String lastName, Address address, int phoneNumber) {
+		// TODO
+	}
+	
+	public void createRentalOffice(Address address, int phoneNumber) {
+		// TODO
+	}
+	
+	public void session() {
+		//TODO
+	}
+	
+	private ArrayList<RentalOffice> getOffices(String location){
+		// TODO
+		return null;
+	}
+	
+	private ArrayList<Car> searchQuery(RentalOffice office, Date time){
+		// TODO
+		return null;
+	}
+	
+	private void presentCars() {
+		//TODO
+	}
+	
+	public boolean pickUpCar(String location, int reservationId) {
+		// TODO
+		return true;
+	}
+	
+	public boolean dropOffCar(String location, int reservationId) {
+		// TODO
+		return true;
+	}
+	
+	private boolean makeReservation(RentalOffice office, Car car, Customer customer, Date time) {
+		// TODO
+		return true;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public ArrayList<Customer> getCustomers() {
+		return customers;
+	}
+	public void setCustomers(ArrayList<Customer> customers) {
+		this.customers = customers;
+	}
+	public ArrayList<RentalOffice> getOffices() {
+		return offices;
+	}
+	public void setOffices(ArrayList<RentalOffice> offices) {
+		this.offices = offices;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	
+}
