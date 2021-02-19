3ea5cdef341a6cc83d8cc6f1021d858412d2e339
Ehrensverd <mail.eskil@gmail.com>
2021-02-19 09:54:25 +0100

    Add docstrings

--- src/carRental/CarRental.java
+++ src/carRental/CarRental.java
@@ -5,138 +5,174 @@ import java.util.*;
 
 public class CarRental {
 
-	private String name;
-	private int phoneNumber;
-	private ArrayList<Customer> customers;
-	private ArrayList<RentalOffice> offices;
-	private Address address;
-	private HashMap<Integer,Reservation> reservationsMap;
-	private int nextReservationID;
-	
-	public CarRental(String name, int phoneNumber, ArrayList<Customer> customers, ArrayList<RentalOffice> offices,
-			Address address) {
-		super();
-		this.name = name;
-		this.phoneNumber = phoneNumber;
-		this.customers = customers;
-		this.offices = offices;
-		this.address = address;
-		this.nextReservationID = 0;
-		this.reservationsMap = new HashMap<>();
-	}
+    private String name;
+    private int phoneNumber;
+    private ArrayList<Customer> customers;
+    private ArrayList<RentalOffice> offices;
+    private Address address;
+    private HashMap<Integer, Reservation> reservationsMap;
+    private int nextReservationID;
 
-	public int giveNextReservationID(){
+    public CarRental(String name, int phoneNumber, ArrayList<Customer> customers, ArrayList<RentalOffice> offices,
+                     Address address) {
+        super();
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.customers = customers;
+        this.offices = offices;
+        this.address = address;
+        this.nextReservationID = 0;
+        this.reservationsMap = new HashMap<>();
+    }
 
-		return this.nextReservationID++;
+    public int giveNextReservationID() {
 
-	}
-	
-	public CarRental() {
-		
-	}
-	
-	public void createCustomer(String firstName, String lastName, Address address, int phoneNumber) {
-		this.customers.add(new Customer(firstName,lastName,address,phoneNumber));
-	}
-	
-	public void createRentalOffice(Address address, int phoneNumber) {
-		// TODO
-	}
-	
-	public void session() {
-		//TODO
-	}
-	
-	private ArrayList<RentalOffice> findOffices(String location){
-		ArrayList<RentalOffice> locations = new ArrayList<RentalOffice>();
-		for(RentalOffice office : this.getOffices()){
-			if (office.getAddress().getCity().toLowerCase().equals(location.toLowerCase())) {
-				locations.add(office);
-			}
-		}
-		return locations;
-	}
-	
-	private ArrayList<Car> searchQuery(RentalOffice office, LocalDateTime pickUpTime, LocalDateTime delivieryDueDate){
+        return this.nextReservationID++;
 
+    }
 
-		Set<Car> carSet = new HashSet<>(office.getCarPark());
-		for(Reservation reservation : office.getReservations()){
-			if (pickUpTime.isAfter(reservation.getPickUpDate()) && pickUpTime.isBefore(reservation.getDeliveryDueDate()) ){
-				if(delivieryDueDate.isAfter(reservation.getPickUpDate()) && delivieryDueDate.isBefore(reservation.getDeliveryDueDate())){
-					carSet.remove(reservation.getCar());
+    public CarRental() {
 
-				}
-			}
+    }
 
+    /**
+     * <p>Create and add customer to customers ArrayList</p>
+     *
+     * @param firstName
+     * @param lastName
+     * @param address
+     * @param phoneNumber
+     */
+    public void createCustomer(String firstName, String lastName, Address address, int phoneNumber) {
+        this.customers.add(new Customer(firstName, lastName, address, phoneNumber));
+    }
 
-		}
+    public void createRentalOffice(Address address, int phoneNumber) {
+        // TODO
+    }
 
-		return  new ArrayList<>(carSet);
+    public void session() {
+        //TODO
+    }
 
-	}
-	
-	private void presentCars() {
-		//TODO
-	}
-	
-	public boolean pickUpCar(String location, int reservationID) {
+    /**
+     * <p>Searches for all RentalOffice in offices that match given location</p>
+     *
+     * @param location
+     * @return all matching RentalOffices
+     */
+    private ArrayList<RentalOffice> findOffices(String location) {
+        ArrayList<RentalOffice> locations = new ArrayList<RentalOffice>();
+        for (RentalOffice office : this.getOffices()) {
+            if (office.getAddress().getCity().toLowerCase().equals(location.toLowerCase())) {
+                locations.add(office);
+            }
+        }
+        return locations;
+    }
 
-		Reservation reservation = this.reservationsMap.get(reservationID);
-		if ((Integer)reservation.getCustomer().getCardNumber() == null){
-			return false;
-		}
+    /**
+     * <p>Finds all available cars at given RentalOffice for given time period</p>
+     *
+     * @param office
+     * @param pickUpTime
+     * @param delivieryDueDate
+     * @return
+     */
+    private ArrayList<Car> searchQuery(RentalOffice office, LocalDateTime pickUpTime, LocalDateTime delivieryDueDate) {
 
 
-		return true;
-	}
-	
-	public int dropOffCar(int dropOffOffice, int reservationID) {
-		Reservation reservation = this.reservationsMap.get(reservationID);
-		Optional<RentalOffice> office =  getOffices().stream().filter(off -> off.getOfficeId()==dropOffOffice).findFirst();
-		int price = office.get().dropOffEvent(reservation);
-		return price;
-	}
-	
-	private boolean makeReservation(RentalOffice office, Car car, Customer customer, LocalDateTime pickUpDate, LocalDateTime dropOffDate) {
-		// TODO
-		return true;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getPhoneNumber() {
-		return phoneNumber;
-	}
-	public void setPhoneNumber(int phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	public ArrayList<Customer> getCustomers() {
-		return customers;
-	}
-	public void setCustomers(ArrayList<Customer> customers) {
-		this.customers = customers;
-	}
-	public ArrayList<RentalOffice> getOffices() {
-		return offices;
-	}
-	public void setOffices(ArrayList<RentalOffice> offices) {
-		this.offices = offices;
-	}
-	public Address getAddress() {
-		return address;
-	}
-	public void setAddress(Address address) {
-		this.address = address;
-	}
+        Set<Car> carSet = new HashSet<>(office.getCarPark());
+        for (Reservation reservation : office.getReservations()) {
 
-	public HashMap<Integer, Reservation> getReservationsMap(){
-		return this.reservationsMap;
-	}
-	
-	
+            // Only need to check cars still in carSet
+            if (carSet.contains(reservation.getCar())) {
+
+                // Check and remove car if queried pickup and drop off time is within the reservation time period
+                if (pickUpTime.isAfter(reservation.getPickUpDate()) && pickUpTime.isBefore(reservation.getDeliveryDueDate())) {
+                    if (delivieryDueDate.isAfter(reservation.getPickUpDate()) && delivieryDueDate.isBefore(reservation.getDeliveryDueDate())) {
+                        carSet.remove(reservation.getCar());
+
+                    }
+                }
+            }
+
+
+        }
+
+        return new ArrayList<>(carSet);
+
+    }
+
+    private void presentCars() {
+        //TODO
+    }
+
+
+    public boolean pickUpCar(String location, int reservationID) {
+
+        Reservation reservation = this.reservationsMap.get(reservationID);
+        if ((Integer) reservation.getCustomer().getCardNumber() == null) {
+            return false;
+        }
+        return true;
+    }
+
+    public int dropOffCar(int dropOffOffice, int reservationID) {
+        Reservation reservation = this.reservationsMap.get(reservationID);
+        Optional<RentalOffice> office = getOffices().stream().filter(off -> off.getOfficeId() == dropOffOffice).findFirst();
+        int price = office.get().dropOffEvent(reservation);
+        return price;
+    }
+
+    private boolean makeReservation(RentalOffice office, Car car, Customer customer, LocalDateTime pickUpDate, LocalDateTime dropOffDate) {
+        // TODO
+        return true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(ArrayList<Customer> customers) {
+        this.customers = customers;
+    }
+
+    public ArrayList<RentalOffice> getOffices() {
+        return offices;
+    }
+
+    public void setOffices(ArrayList<RentalOffice> offices) {
+        this.offices = offices;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public HashMap<Integer, Reservation> getReservationsMap() {
+        return this.reservationsMap;
+    }
+
+
 }
